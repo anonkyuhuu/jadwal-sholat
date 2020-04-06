c100a4d2adcd94dc195ff0f744b634e18689a907
anonkyuhuu <irsyad.swasono@yahoo.com>
2020-04-06 13:45:17 +0700

    First Commit

---
+++ README.md
@@ -0,0 +1,13 @@
+# jadwal-sholat
+Tool jadwal sholat in cli
+
+# instal7l
+$ apt-get update
+$ apt install git python
+$ pip2 install requests re
+$ git clone https://github.com/anonkyuhuu/jadwal-sholat
+$ cd jadwal-sholat
+$ python3 main.py jakarta
+
+Note:
+jakarta ganti dengan kota kamu
---
+++ main.py
@@ -0,0 +1,45 @@
+import requests, sys, re, os
+
+os.system('clear')
+banner ="""
+   _____
+  /  '  \     [ PRAYER SCHEDULE ]
+ |   |   |
+ |- -•  -|     Author  : sCuby07
+ |       |   |==================|
+  \__'__/         Version 0.1"""
+
+def help():
+	exit('''
+This tool only use python3
+
+usage:
+python3 main.py [city]
+
+ex:
+python3 main.py jakarta''')
+
+
+try:
+	x = requests.get('http://muslimsalat.com/%s'%(sys.argv[1])).text
+	s = re.search('"title":"(.*?)"', x).group(1)
+	z = r'{"date_for":"(.*?)","fajr":"(.*?)","shurooq":"(.*?)","dhuhr":"(.*?)","asr":"(.*?)","maghrib":"(.*?)","isha":"(.*?)"}'
+	a = re.search(z, x)
+	print(banner)
+	if s == '':
+		b = re.search('"query":"(.*?)"', x).group(1)
+		b = b.replace(b[0], b[0].upper())
+		print('\nCity : %s, Indonesia'% (b))
+	else:
+		print('\nCity : %s'% (s))
+	print('''Date : %s
+Fajr : %s
+Shurooq : %s
+Dhuhr : %s
+Asr : %s
+Maghrib : %s
+Isha : %s'''%(a.group(1),a.group(2),a.group(3),a.group(4),a.group(5),a.group(6),a.group(7)))
+except KeyboardInterrupt:
+	pass
+except:
+	help()
